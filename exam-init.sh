b8327911b941daf364297c3337ba96de6978da90
JZhengJarvis <jzheng@mail.sim.ac.cn>
2022-10-13 21:43:34 +0800

    fsimg copy v2

--- exam-init.sh
+++ exam-init.sh
@@ -3,14 +3,18 @@ cd ..  # go up to the ysyx-workbench directory
 d="apps/pal/repo"
 if [[ ! -d navy-apps/$d ]]; then
   cp -r $NAVY_HOME/$d `dirname navy-apps/$d`
-  mkdir -p navy-apps/fsimg/share/games
+  mkdir -p navy-apps/fsimg/share/games/pal
+  cp -r $NAVY_HOME/$d/data/. ./navy-apps/fsimg/share/games/pal
 else
   echo navy-apps/$d already exists
 fi
+
 d="apps/bird/repo"
 if [[ ! -d navy-apps/$d ]]; then
   cp -r $NAVY_HOME/$d `dirname navy-apps/$d`
-  mkdir -p navy-apps/fsimg/share/games
+  mkdir -p navy-apps/fsimg/share/games/bird
+  cp -r $NAVY_HOME/$d/res/. ./navy-apps/fsimg/share/games/bird
+
 else
   echo navy-apps/$d already exists
 fi
@@ -33,6 +37,8 @@ if [[ ! -d nemu/$d ]]; then
   cp -r $NEMU_HOME/$d `dirname nemu/$d`
 else
   echo `dirname nemu/$d` already exists
+  rm -r ./am-kernels 
+  cp -r $NEMU_HOME/$d ./am-kernels
 fi
 
 source ysyx-exam/setup-env.sh
@@ -45,6 +51,3 @@ make -C $NEMU_HOME -j distclean
 touch $NEMU_HOME/.config
 cp ysyx-exam/exam_defconfig $NEMU_HOME/configs/
 make -C $NEMU_HOME exam_defconfig
-
-cp -r $NAVY_HOME/apps/pal/repo/data $NAVY_HOME/fsimg/share/games/pal
-cp -r $NAVY_HOME/apps/bird/repo/res $NAVY_HOME/fsimg/share/games/bird
